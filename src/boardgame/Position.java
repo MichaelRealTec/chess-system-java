8df18e0817476e820bcc54f3d7b26389cec2dade
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-11-30 16:56:41 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,38 @@
+package boardgame;
+
+public class Position {
+	// Atributos
+	private int row;      // row = linha
+	private int column;   // column = coluna
+	
+	// Métodos Getters e Settesr
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	
+	// Método Construtor
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	// Métodos
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
