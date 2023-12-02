31941a9c2a86b17aec492c408b210cbdd41a5919
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-02 00:13:56 -0300

    Handling exception and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -33,6 +33,12 @@ public class UI {
 	
 	
 	// Métodos 
+	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
