aca64ed25dc27d52df5f9fbac78842f80e44c7ca
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-06 17:21:48 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,14 +7,20 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 	// Atributos
 	private Color color;
+	private int moveCount;
 	
 	// Método Getter e Setter
 	public Color getColor() {
 		return color;
 	}
 	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position); 
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
 	}
 	
 	// Método construtor
@@ -28,5 +34,14 @@ public abstract class ChessPiece extends Piece {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
 
 }
